15b472bcfe9b7a5d75fe762afe4b549d853c9f90
Yongwoo Lee <yw6636@gmail.com>
2023-05-20 13:59:23 -0400

    Finsihed all of the tasks given

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -86,11 +86,64 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-    
+    const { title, contents } = req.body
+    if(!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    }
+    else {
+        Post.findById(req.params.id)
+            .then(stuff => {
+                if (!stuff) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist"
+                    })
+                }
+                else{
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if (data) {
+                    return Post.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                if (post) {
+                    res.json(post)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'The posts information could not be retrieved',
+                    err: err.message,
+                    stack: err.stack
+                })
+            })
+    }
 })
 
-router.get('/:id/messages', (req, res) => {
-    
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        }
+        else {
+            const messages = await Post.findPostComments(req.params.id)
+            res.json(messages)
+        }
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'The comment information could not be retrieved',
+            err: err.mesasage,
+            stack: err.stack
+        })
+    }
 })
 
 module.exports = router
